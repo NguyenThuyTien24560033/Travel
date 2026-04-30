fbc96778fe9a5f6a88b34406086214b226b366af
Nguyen-Dinh-Khang <khang.nguyen.dinh.2401@gmail.com>
2026-04-30 23:36:57 +0700

    cập nhật (30-4)

--- travel-plan-kntv/src/assets/Layouts/UserLayout.jsx
+++ travel-plan-kntv/src/assets/Layouts/UserLayout.jsx
@@ -16,12 +16,12 @@ export const useUser = () => useContext(UserContext);
 ========================================================= */
 
 
-// const MODE = "JSON_SERVER"; 
-// const JSON_API = "http://localhost:3001/users";
+const MODE = "JSON_SERVER"; 
+const JSON_API = "http://localhost:3001/users";
 
 
 // Đình Khang đổi comment khi chạy backend thật
-const MODE = "REAL_BACKEND";
+// const MODE = "REAL_BACKEND";
 const REAL_API = {
   login: "http://localhost:8000/travel/api/login/",
   register: "http://localhost:8000/travel/users/",
@@ -52,6 +52,7 @@ const api = {
 
     const res = await fetch(REAL_API.login, {
       method: "POST",
+      credentials: 'include',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
